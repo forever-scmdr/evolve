0b4b929b50c96f6f9082733b2ed5590c43bc4392
Egor <georgy.karlov@gmail.com>
2018-02-04 01:06:11 +0300

    Работа над киентом для скачки во время парсинга

--- src/ecommander/fwk/WebClient.java
+++ src/ecommander/fwk/WebClient.java
@@ -1,28 +1,40 @@
 package ecommander.fwk;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.fluent.Request;
-import org.w3c.dom.Document;
+import org.apache.http.entity.ContentType;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * Класс для закачки файлов по урлу
  * Created by E on 2/2/2018.
  */
 public class WebClient {
-	public static String getString(String url, String encoding) {
-		Request.Get(url)
+	public static String getString(String url) throws IOException {
+		return Request.Get(url)
 				.addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
 				.connectTimeout(1000).socketTimeout(3000)
 				.execute()
-				.handleResponse(new ResponseHandler<Document>() {
-					@Override
-					public Document handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
+				.handleResponse(response -> {
+					StatusLine statusLine = response.getStatusLine();
+					HttpEntity entity = response.getEntity();
+					if (statusLine.getStatusCode() >= 300) {
+						throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
+					}
+					if (entity == null) {
 						return null;
 					}
+					ContentType contentType = ContentType.getOrDefault(entity);
+					Charset charset = contentType.getCharset();
+					if (charset == null) {
+						charset = Charset.defaultCharset();
+					}
+					return IOUtils.toString(entity.getContent(), charset);
 				});
 	}
 }
