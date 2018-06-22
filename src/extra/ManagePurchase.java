8a3bfb3ec78629956f8b67ec20d832b5a179b781
Egor <georgy.karlov@gmail.com>
2018-06-22 10:59:31 +0300

    testo

--- src/extra/ManagePurchase.java
+++ src/extra/ManagePurchase.java
@@ -27,43 +27,36 @@ public class ManagePurchase extends Command implements ItemNames {
 	@Override
 	public ResultPE execute() throws Exception {
 		try {
-			final byte managesId = ItemTypeRegistry.getAssocId(MANAGES_ASSOC);
-			long purchaseId = Long.parseLong(getVarSingleValue("p"));
-			String managerEmail = getVarSingleValue("manager");
-			String statusString = getVarSingleValue("status");
-			Item purch = ItemQuery.loadById(purchaseId);
-			if (purch == null) {
-				return getResult(ERROR).setVariable(MESSAGE, "Не найден требуемый заказ");
+			String purchaseIdStr = getVarSingleValue("p");
+			String userIdStr = getVarSingleValue("u");
+			if (StringUtils.isNotBlank(purchaseIdStr)) {
+				long purchaseId = Long.parseLong(purchaseIdStr);
+				String managerEmail = getVarSingleValue("manager");
+				String statusString = getVarSingleValue("status");
+				Item purch = ItemQuery.loadById(purchaseId);
+				if (purch == null) {
+					return getResult(ERROR).setVariable(MESSAGE, "Не найден требуемый заказ");
+				}
+				if (StringUtils.isNotBlank(managerEmail)) {
+					// Загрузка пользователя заказа
+					Item purchaseUser = new ItemQuery(USER).setChildId(purchaseId, false, ItemTypeRegistry.getPrimaryAssoc().getName()).loadFirstItem();
+					if (purchaseUser == null) {
+						return getResult(ERROR).setVariable(MESSAGE, "Некорректный заказ, отсутствует заказчик");
+					}
+					return setNewManager(purchaseUser, managerEmail);
+				} else if (StringUtils.isNotBlank(statusString)) {
+					purch.setValueUI(purchase.STATUS, statusString);
+					executeAndCommitCommandUnits(SaveItemDBUnit.get(purch));
+					return getResult(SUCCESS).setVariable(MESSAGE, "Статус заказа изменен");
+				}
 			}
-			if (StringUtils.isNotBlank(managerEmail)) {
-				// Загрузка и проверка нового менежера
-				Item newOwnerItem = new ItemQuery(MANAGER).addParameterCriteria(manager.EMAIL, managerEmail, "=", null, Compare.SOME).loadFirstItem();
-				if (newOwnerItem == null) {
-					return getResult(ERROR).setVariable(MESSAGE, "Некорректный логин менеджера");
+			else if (StringUtils.isNotBlank(userIdStr)) {
+				long userId = Long.parseLong(userIdStr);
+				Item user = ItemQuery.loadById(userId);
+				if (user == null) {
+					return getResult(ERROR).setVariable(MESSAGE, "Не найден требуемый пользователь");
 				}
-				// Загрузка пользователя заказа
-				Item purchaseUser = new ItemQuery(USER).setChildId(purchaseId, false, ItemTypeRegistry.getPrimaryAssoc().getName()).loadFirstItem();
-				if (purchaseUser == null) {
-					return getResult(ERROR).setVariable(MESSAGE, "Некорректный заказ, отсутствует заказчик");
-				}
-				// Загрузка для удаления старого менеджера заказчика
-				Item oldOwnerItem = new ItemQuery(MANAGER).setChildId(purchaseUser.getId(), false, MANAGES_ASSOC).loadFirstItem();
-				// Если старый и новый менеджеры одинаковые, ничего делать не надо
-				if (oldOwnerItem != null && newOwnerItem.getId() == oldOwnerItem.getId()) {
-					return getResult(SUCCESS).setVariable(MESSAGE, "Изменение менеджера не требуется");
-				}
-				// Удаление старого менеджера
-				if (oldOwnerItem != null) {
-					executeCommandUnit(new DeleteAssocDBUnit(purchaseUser, oldOwnerItem, managesId));
-				}
-				// Прикрепление нового менеджера
-				executeCommandUnit(new CreateAssocDBUnit(purchaseUser, newOwnerItem, managesId, false));
-				commitCommandUnits();
-				return getResult(SUCCESS).setVariable(MESSAGE, "Заказчику присвоен новый менеджер");
-			} else if (StringUtils.isNotBlank(statusString)) {
-				purch.setValueUI(purchase.STATUS, statusString);
-				executeAndCommitCommandUnits(SaveItemDBUnit.get(purch));
-				return getResult(SUCCESS).setVariable(MESSAGE, "Статус заказа изменен");
+				return setNewManager(user, getVarSingleValue("manager"));
 			}
 			return getResult(ERROR).setVariable(MESSAGE, "Не передана необходимая для выполения действия информация");
 		} catch (Exception e) {
@@ -71,5 +64,35 @@ public class ManagePurchase extends Command implements ItemNames {
 		}
 
 		return getResult(ERROR).setVariable(MESSAGE, "Ошибка изменения заказа");
+	}
+
+	/**
+	 * Установить нового менеждера для заказчика
+	 * @param purchaseUser
+	 * @param managerEmail
+	 * @return
+	 * @throws Exception
+	 */
+	private ResultPE setNewManager(Item purchaseUser, String managerEmail) throws Exception {
+		final byte managesId = ItemTypeRegistry.getAssocId(MANAGES_ASSOC);
+		// Загрузка и проверка нового менежера
+		Item newOwnerItem = new ItemQuery(MANAGER).addParameterCriteria(manager.EMAIL, managerEmail, "=", null, Compare.SOME).loadFirstItem();
+		if (newOwnerItem == null) {
+			return getResult(ERROR).setVariable(MESSAGE, "Некорректный логин менеджера");
+		}
+		// Загрузка для удаления старого менеджера заказчика
+		Item oldOwnerItem = new ItemQuery(MANAGER).setChildId(purchaseUser.getId(), false, MANAGES_ASSOC).loadFirstItem();
+		// Если старый и новый менеджеры одинаковые, ничего делать не надо
+		if (oldOwnerItem != null && newOwnerItem.getId() == oldOwnerItem.getId()) {
+			return getResult(SUCCESS).setVariable(MESSAGE, "Изменение менеджера не требуется");
+		}
+		// Удаление старого менеджера
+		if (oldOwnerItem != null) {
+			executeCommandUnit(new DeleteAssocDBUnit(purchaseUser, oldOwnerItem, managesId));
+		}
+		// Прикрепление нового менеджера
+		executeCommandUnit(new CreateAssocDBUnit(purchaseUser, newOwnerItem, managesId, false));
+		commitCommandUnits();
+		return getResult(SUCCESS).setVariable(MESSAGE, "Заказчику присвоен новый менеджер");
 	}
 }
