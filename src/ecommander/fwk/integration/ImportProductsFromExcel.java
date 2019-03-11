c484f80cf3c6bd27dc141c85eee4666bd4da96b3
antonStezhkin <bio@forever-ds.com>
2019-03-11 13:55:42 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/ecommander/fwk/integration/ImportProductsFromExcel.java
+++ src/ecommander/fwk/integration/ImportProductsFromExcel.java
@@ -27,6 +27,7 @@ public class ImportProductsFromExcel extends CreateParametersAndFiltersCommand i
 	Item currentSubsection;
 	private boolean newItemTypes = false;
 	private HashSet<Long> sectionsWithNewItemTypes = new HashSet<>();
+	private HashSet<String> duplicateCodes = new HashSet<>();
 	//page vars
 	//private static final String SEC_VAR = "sec";
 	private static final String WITH_EXISTING_SECS = "with_existing_sections"; // what to do with existing sections (UPDATE, COPY, CREATE, COPY_IF_PARENT_DIFFERS, MOVE_IF_PARENT_DIFFERS, DELETE)
@@ -87,7 +88,15 @@ public class ImportProductsFromExcel extends CreateParametersAndFiltersCommand i
 				String code = getValue(CreateExcelPriceList.CODE_FILE);
 				String name = getValue(CreateExcelPriceList.NAME_FILE);
 				if (StringUtils.isBlank(code)) return;
-				else if (StringUtils.startsWith(code, "разд:")) {
+
+				//check duplicate codes
+				if(duplicateCodes.contains(code)){
+					info.addError("Повторяющийся артикул", code);
+				}else{
+					duplicateCodes.add(code);
+				}
+
+				if (StringUtils.startsWith(code, "разд:")) {
 					code = StringUtils.substringAfter(code, "разд:").trim();
 					String parentCode = name;
 					name = getValue(CreateExcelPriceList.PRICE_FILE);
