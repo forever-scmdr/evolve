f74f096e25ed6abb6c51413f768a16ac651aa37a
antonStezhkin <bio@forever-ds.com>
2019-03-12 13:12:58 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/ecommander/fwk/integration/UpdatePricesFromExcel.java
+++ src/ecommander/fwk/integration/UpdatePricesFromExcel.java
@@ -25,22 +25,40 @@ public class UpdatePricesFromExcel extends IntegrateBase implements CatalogConst
 		catalog = ItemQuery.loadSingleItemByName(CATALOG_ITEM);
 		String repository = AppContext.getFilesDirPath(catalog.isFileProtected());
 		File priceList = catalog.getFileValue(INTEGRATION_PARAM, repository);
-		priceWB = new ExcelPriceList(priceList, CreateExcelPriceList.CODE_FILE, CreateExcelPriceList.NAME_FILE, CreateExcelPriceList.PRICE_FILE, CreateExcelPriceList.QTY_FILE, CreateExcelPriceList.AVAILABLE_FILE) {
+		priceWB = new ExcelPriceList(priceList, CreateExcelPriceList.CODE_FILE, CreateExcelPriceList.PRICE_FILE) {
 			private int rowNum = 0;
 			@Override
 			protected void processRow() throws Exception {
 				String code = getValue(CreateExcelPriceList.CODE_FILE);
 				if(StringUtils.isBlank(code) || CreateExcelPriceList.CODE_FILE.equalsIgnoreCase(code) || StringUtils.startsWith(code,"разд:")) return;
-				boolean isLineProduct = code.indexOf('@') != -1;
-				code = (isLineProduct)? code.substring(0, code.indexOf('@')) : code;
 				String price = getValue(CreateExcelPriceList.PRICE_FILE);
 				String qty = getValue(CreateExcelPriceList.QTY_FILE);
 				String av = getValue(CreateExcelPriceList.AVAILABLE_FILE);
+				String oldPrice = getValue(CreateExcelPriceList.PRICE_OLD_FILE);
+				String origPrice = getValue(CreateExcelPriceList.PRICE_ORIGINAL_FILE);
+				String currency = getValue(CreateExcelPriceList.CURRENCY_ID_FILE);
+				String unit = getValue(CreateExcelPriceList.UNIT_FILE);
 				Item product = ItemQuery.loadSingleItemByParamValue(ItemNames.PRODUCT, CODE_PARAM, code);
 				if(product != null){
-					product.setValueUI(PRICE_PARAM, price);
-					product.setValueUI(QTY_PARAM, qty);
-					product.setValueUI(AVAILABLE_PARAM, av);
+					product.setValueUI(PRICE_PARAM, price.replaceAll("[^\\d,.]",""));
+					if(qty != null) {
+						product.setValueUI(QTY_PARAM, qty);
+					}
+					if(av != null) {
+						product.setValueUI(AVAILABLE_PARAM, av);
+					}
+					if(oldPrice != null){
+						product.setValueUI(PRICE_OLD_PARAM, oldPrice.replaceAll("[^\\d,.]",""));
+					}
+					if(origPrice != null){
+						product.setValueUI(PRICE_ORIGINAL_PARAM, origPrice.replaceAll("[^\\d,.]",""));
+					}
+					if(currency != null) {
+						product.setValueUI(CURRENCY_ID_PARAM, currency);
+					}
+					if(unit != null) {
+						product.setValueUI(CURRENCY_ID_PARAM, currency);
+					}
 					DelayedTransaction.executeSingle(User.getDefaultUser(), SaveItemDBUnit.get(product).noFulltextIndex().noTriggerExtra());
 					setProcessed(rowNum++);
 				}
