eb36d176b0cfd6e1733929f13e8716e9f137cd04
Egor <georgy.karlov@gmail.com>
2019-03-12 18:42:43 +0300

    Фикс создания фильтров

--- src/ecommander/fwk/integration/CreateParametersAndFiltersCommand.java
+++ src/ecommander/fwk/integration/CreateParametersAndFiltersCommand.java
@@ -236,7 +236,8 @@ public class CreateParametersAndFiltersCommand extends IntegrateBase implements 
 			if (products.size() > 0) {
 
 				// Загрузить и добавить все строковые товары
-				products.addAll(new ItemQuery(LINE_PRODUCT_ITEM).setParentId(section.getId(), true).loadItems());
+				if (ItemTypeRegistry.getItemType(LINE_PRODUCT_ITEM) != null)
+					products.addAll(new ItemQuery(LINE_PRODUCT_ITEM).setParentId(section.getId(), true).loadItems());
 
 				for (Item product : products) {
 					Item paramsXml = new ItemQuery(PARAMS_XML_ITEM).setParentId(product.getId(), false).loadFirstItem();
