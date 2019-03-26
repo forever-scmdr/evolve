8d7b85e7ab7e902e33fba4ab174d3d7e9b5d7f47
Egor <georgy.karlov@gmail.com>
2019-03-26 16:04:04 +0300

    Фиксы metabo 2 парсинг

--- src/extra/MetaboIntegrateParsedCommand.java
+++ src/extra/MetaboIntegrateParsedCommand.java
@@ -2,9 +2,12 @@ package extra;
 
 import ecommander.fwk.*;
 import ecommander.model.*;
+import ecommander.persistence.commandunits.CopyItemDBUnit;
+import ecommander.persistence.commandunits.CreateAssocDBUnit;
 import ecommander.persistence.commandunits.ItemStatusDBUnit;
 import ecommander.persistence.commandunits.SaveItemDBUnit;
 import ecommander.persistence.itemquery.ItemQuery;
+import extra._generated.Section;
 import lunacrawler.fwk.ParsedInfoProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
@@ -87,6 +90,8 @@ public class MetaboIntegrateParsedCommand extends IntegrateBase {
 	}
 
 	private void processSubsections(Element root, Item parent) throws Exception {
+		if (needTermination)
+			return;
 		Elements sectionEls = root.select("> section");
 		for (Element sectionEl : sectionEls) {
 			String[] path = StringUtils.split(sectionEl.attr(ID), '_');
@@ -110,121 +115,138 @@ public class MetaboIntegrateParsedCommand extends IntegrateBase {
 				ServerLogger.error("Error parsing product xml file", e);
 				info.addError("Документ для товара '" + code + "' содержит ошибки", code);
 			}
-			String name = productDoc.getElementsByTag(NAME).first().ownText();
-			String type = productDoc.getElementsByTag(TYPE).first().ownText();
-			String nameExtra = productDoc.getElementsByTag(NAME_EXTRA).first().ownText();
-			String shortTxt = productDoc.getElementsByTag(SHORT).first().html();
-			String description = productDoc.getElementsByTag(EXTRA).first().html();
-			String text = productDoc.getElementsByTag(DESCRIPTION).first().html();
-			String tech = productDoc.getElementsByTag(TECH).first().html();
-			String packageTxt = productDoc.getElementsByTag(PACKAGE).first().html();
-			String extraXml = productDoc.getElementsByTag(SYMBOLS).first().html();
-			Elements spinEls = productDoc.getElementsByTag(SPIN);
-			for (Element spinEl : spinEls) {
-				extraXml += spinEl.outerHtml();
-			}
-			Elements videoEls = productDoc.getElementsByTag(VIDEO);
-			for (Element videoEl : videoEls) {
-				extraXml += videoEl.outerHtml();
-			}
-			Elements manuals = productDoc.getElementsByTag(MANUAL);
-			if (manuals.size() > 0) {
-				extraXml += manuals.first().outerHtml();
-			}
-			Elements spareParts = productDoc.getElementsByTag(PARTS);
-			if (spareParts.size() > 0) {
-				extraXml += spareParts.first().outerHtml();
-			}
-			ArrayList<Path> gallery = new ArrayList<>();
-			Elements pics = productDoc.getElementsByTag(GALLERY).first().getElementsByTag(PIC);
-			for (Element pic : pics) {
-				Path file = infoProvider.getFile(code, pic.attr(LINK));
-				if (file != null)
-					gallery.add(file);
-			}
-			ArrayList<String> assocCodes = new ArrayList<>();
-			Elements codeEls = productDoc.getElementsByTag(ASSOC).first().getElementsByTag(CODE);
-			for (Element codeEl : codeEls) {
-				assocCodes.add(codeEl.ownText());
-			}
-			// Продукт
-
-			Item product = Item.newChildItem(productType, parent);
-			product.setValue(NAME, name);
-			product.setValue(CODE, code);
-			product.setValue(TYPE, type);
-			product.setValue(NAME_EXTRA, nameExtra);
-			product.setValue(SHORT, shortTxt);
-			product.setValue(DESCRIPTION, description);
-			product.setValue(TEXT, text);
-			product.setValue(EXTRA_XML, extraXml);
-			for (String assocCode : assocCodes) {
-				product.setValue(ASSOC_CODE, assocCode);
-			}
-			for (Path path : gallery) {
-				product.setValue(GALLERY, path.toFile());
-			}
-			if (gallery.size() > 0) {
-				Path firstPic = gallery.get(0);
-				Path newMainPic = firstPic.resolveSibling("main_" + firstPic.getFileName());
- 				ByteArrayOutputStream bos = ResizeImagesFactory.resize(firstPic.toFile(), 0, 400);
-				Files.write(newMainPic, bos.toByteArray());
-				product.setValue(MAIN_PIC, newMainPic.toFile());
-			}
-
-			executeAndCommitCommandUnits(SaveItemDBUnit.get(product).noFulltextIndex().ignoreFileErrors());
-
-			// Параметры XML
-
-			XmlDocumentBuilder xml = XmlDocumentBuilder.newDocPart();
-			if (StringUtils.isNotBlank(tech)) {
-				Element techEl = productDoc.getElementsByTag(TECH).first().getElementById("attributes");
-				Elements childrenDivs = techEl.children();
-				for (Element div : childrenDivs) {
-					String divClass = div.attr("class");
-					if (StringUtils.equalsIgnoreCase(divClass, "attributesGroup")) {
-						xml.endElement();
-						xml.startElement("group").startElement("name").addText(div.ownText()).endElement();
-					} else if (StringUtils.equalsIgnoreCase(divClass, "attributes")) {
-						Elements rows = div.children();
-						for (Element row : rows) {
-							Elements nameValue = row.children();
-							if (nameValue.size() > 0) {
-								String paramName = nameValue.get(0).ownText();
-								xml.startElement("parameter").startElement("name").addText(paramName).endElement();
-								if (nameValue.size() > 1) {
-									String paramValue = nameValue.get(1).ownText();
-									xml.startElement("value").addText(paramValue).endElement();
-								}
-								xml.endElement(); // параметр закрывается
-							}
-						}
-						xml.endElement(); // группа закрывается
-					}
-				}
-
-				Item paramsXml = Item.newChildItem(paramsXmlType, product);
-				paramsXml.setValue(XML, xml.toString());
-				executeAndCommitCommandUnits(SaveItemDBUnit.get(paramsXml).noFulltextIndex().ignoreFileErrors());
-
-				Item techItem = Item.newChildItem(productExtraType, product);
-				techItem.setValue(NAME, "tech");
-				techItem.setValue(TEXT, tech);
-				executeAndCommitCommandUnits(SaveItemDBUnit.get(techItem).noFulltextIndex().ignoreFileErrors());
-			}
-
-			// комплектность поставки
-
-			if (StringUtils.isNotBlank(packageTxt)) {
-				Item packageItem = Item.newChildItem(productExtraType, product);
-				packageItem.setValue(NAME, "package");
-				packageItem.setValue(TEXT, packageTxt);
-				executeAndCommitCommandUnits(SaveItemDBUnit.get(packageItem).noFulltextIndex().ignoreFileErrors());
-			}
-
-			info.increaseProcessed();
+			deployProduct(productDoc, parent);
 		}
 	}
+
+	void deployProduct(Element productEl, Item parent) throws Exception {
+		if (needTermination)
+			return;
+		String code = productEl.getElementsByTag(CODE).first().ownText();
+		String name = productEl.getElementsByTag(NAME).first().ownText();
+		String type = productEl.getElementsByTag(TYPE).first().ownText();
+		String nameExtra = productEl.getElementsByTag(NAME_EXTRA).first().ownText();
+		String shortTxt = productEl.getElementsByTag(SHORT).first().html();
+		String description = productEl.getElementsByTag(EXTRA).first().html();
+		String text = productEl.getElementsByTag(DESCRIPTION).first().html();
+		String tech = productEl.getElementsByTag(TECH).first().html();
+		String packageTxt = productEl.getElementsByTag(PACKAGE).first().html();
+		String extraXml = productEl.getElementsByTag(SYMBOLS).first().html();
+		Elements spinEls = productEl.getElementsByTag(SPIN);
+		for (Element spinEl : spinEls) {
+			extraXml += spinEl.outerHtml();
+		}
+		Elements videoEls = productEl.getElementsByTag(VIDEO);
+		for (Element videoEl : videoEls) {
+			extraXml += videoEl.outerHtml();
+		}
+		Elements manuals = productEl.getElementsByTag(MANUAL);
+		if (manuals.size() > 0) {
+			extraXml += manuals.first().outerHtml();
+		}
+		Elements spareParts = productEl.getElementsByTag(PARTS);
+		if (spareParts.size() > 0) {
+			extraXml += spareParts.first().outerHtml();
+		}
+		ArrayList<Path> gallery = new ArrayList<>();
+		Elements pics = productEl.getElementsByTag(GALLERY).first().getElementsByTag(PIC);
+		for (Element pic : pics) {
+			Path file = infoProvider.getFile(code, pic.attr(LINK));
+			if (file != null)
+				gallery.add(file);
+		}
+		ArrayList<String> assocCodes = new ArrayList<>();
+		Elements codeEls = productEl.getElementsByTag(ASSOC).first().getElementsByTag(CODE);
+		for (Element codeEl : codeEls) {
+			assocCodes.add(codeEl.ownText());
+		}
+		// Продукт
+
+		// Проверка, если айтем существует - создать копию этого продукта
+		Item product = ItemQuery.loadSingleItemByParamValue(Product._NAME, "code", code);
+		if (product != null) {
+			Item primaryParent = new ItemQuery(Section._NAME).setChildId(product.getId(), false).loadFirstItem();
+			if (primaryParent == null || primaryParent.getId() != parent.getId()) {
+				executeCommandUnit(CreateAssocDBUnit.childExistsSoft(product, parent, ItemTypeRegistry.getAssocId("catalog_link")));
+			}
+		} else {
+			product = Item.newChildItem(productType, parent);
+		}
+		product.setValue(NAME, name);
+		product.setValue(CODE, code);
+		product.setValue(TYPE, type);
+		product.setValue(NAME_EXTRA, nameExtra);
+		product.setValue(SHORT, shortTxt);
+		product.setValue(DESCRIPTION, description);
+		product.setValue(TEXT, text);
+		product.setValue(EXTRA_XML, extraXml);
+		for (String assocCode : assocCodes) {
+			product.setValue(ASSOC_CODE, assocCode);
+		}
+		for (Path path : gallery) {
+			product.setValue(GALLERY, path.toFile());
+		}
+		if (gallery.size() > 0) {
+			Path firstPic = gallery.get(0);
+			Path newMainPic = firstPic.resolveSibling("main_" + firstPic.getFileName());
+			ByteArrayOutputStream bos = ResizeImagesFactory.resize(firstPic.toFile(), 0, 400);
+			Files.write(newMainPic, bos.toByteArray());
+			product.setValue(MAIN_PIC, newMainPic.toFile());
+		}
+
+		executeAndCommitCommandUnits(SaveItemDBUnit.get(product).noFulltextIndex().ignoreFileErrors());
+
+		// Параметры XML
+
+		XmlDocumentBuilder xml = XmlDocumentBuilder.newDocPart();
+		if (StringUtils.isNotBlank(tech)) {
+			Element techEl = productEl.getElementsByTag(TECH).first().getElementById("attributes");
+			Elements childrenDivs = techEl.children();
+			for (Element div : childrenDivs) {
+				String divClass = div.attr("class");
+				if (StringUtils.equalsIgnoreCase(divClass, "attributesGroup")) {
+					xml.endElement();
+					xml.startElement("group").startElement("name").addText(div.ownText()).endElement();
+				} else if (StringUtils.equalsIgnoreCase(divClass, "attributes")) {
+					Elements rows = div.children();
+					for (Element row : rows) {
+						Elements nameValue = row.children();
+						if (nameValue.size() > 0) {
+							String paramName = nameValue.get(0).ownText();
+							xml.startElement("parameter").startElement("name").addText(paramName).endElement();
+							if (nameValue.size() > 1) {
+								String paramValue = nameValue.get(1).ownText();
+								xml.startElement("value").addText(paramValue).endElement();
+							}
+							xml.endElement(); // параметр закрывается
+						}
+					}
+					xml.endElement(); // группа закрывается
+				}
+			}
+
+			Item paramsXml = Item.newChildItem(paramsXmlType, product);
+			paramsXml.setValue(XML, xml.toString());
+			executeAndCommitCommandUnits(SaveItemDBUnit.get(paramsXml).noFulltextIndex().ignoreFileErrors());
+
+			Item techItem = Item.newChildItem(productExtraType, product);
+			techItem.setValue(NAME, "tech");
+			techItem.setValue(TEXT, tech);
+			executeAndCommitCommandUnits(SaveItemDBUnit.get(techItem).noFulltextIndex().ignoreFileErrors());
+		}
+
+		// комплектность поставки
+
+		if (StringUtils.isNotBlank(packageTxt)) {
+			Item packageItem = Item.newChildItem(productExtraType, product);
+			packageItem.setValue(NAME, "package");
+			packageItem.setValue(TEXT, packageTxt);
+			executeAndCommitCommandUnits(SaveItemDBUnit.get(packageItem).noFulltextIndex().ignoreFileErrors());
+		}
+
+		info.increaseProcessed();
+	}
+
 
 	@Override
 	protected void terminate() throws Exception {
