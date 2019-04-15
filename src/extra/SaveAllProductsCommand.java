250cb5b268f61152aa3996e468f5409bb97b4288
antonStezhkin <bio@forever-ds.com>
2019-04-15 18:08:43 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

---
+++ src/extra/SaveAllProductsCommand.java
@@ -0,0 +1,45 @@
+package extra;
+
+import ecommander.fwk.IntegrateBase;
+import ecommander.fwk.MysqlConnector;
+import ecommander.fwk.integration.CatalogConst;
+import ecommander.model.Item;
+import ecommander.persistence.commandunits.SaveItemDBUnit;
+import ecommander.persistence.mappers.ItemMapper;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+/**
+ * Created by user on 15.04.2019.
+ */
+public class SaveAllProductsCommand extends IntegrateBase implements CatalogConst{
+	@Override
+	protected boolean makePreparations() throws Exception {
+		return true;
+	}
+
+	@Override
+	protected void integrate() throws Exception
+	{
+		setOperation("Пересохранение товаров");
+		long startID = 0;
+		ArrayList<Item> products;
+		info.setProcessed(0);
+		try(Connection conn = MysqlConnector.getConnection()) {
+			while ((products = ItemMapper.loadByName(PRODUCT_ITEM, 500, startID, conn)).size() > 0) {
+				for (Item product : products) {
+					startID = product.getId();
+					product.forceInitialInconsistent();
+					executeAndCommitCommandUnits(SaveItemDBUnit.get(product, false).noFulltextIndex().noFulltextIndex());
+					info.increaseProcessed();
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void terminate() throws Exception {
+
+	}
+}
