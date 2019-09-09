d3f04c383f1137520a157bc36bb8bab7f78b7954
anton <bio@forever-ds.com>
2019-09-09 13:16:05 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/extra/CartManageCommand.java
+++ src/extra/CartManageCommand.java
@@ -13,7 +13,6 @@ import ecommander.pages.LinkPE;
 import ecommander.pages.MultipleHttpPostForm;
 import ecommander.pages.ResultPE;
 import ecommander.persistence.itemquery.ItemQuery;
-import extra._generated.Discounts;
 import extra._generated.ItemNames;
 import extra._generated.User_jur;
 import org.apache.commons.lang3.StringUtils;
@@ -29,11 +28,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.HashSet;
-import java.util.Locale;
 
 /**
  * Корзина
@@ -95,7 +90,7 @@ public class CartManageCommand extends BasicCartManageCommand {
 		}
 		return !hasError;
 	}
-/*
+
 	@Override
 	protected boolean recalculateCart(String...priceParamName) throws Exception {
 //		if (discounts == null) {
@@ -104,9 +99,7 @@ public class CartManageCommand extends BasicCartManageCommand {
 //					User.getDefaultUser(), common.getId(), common.getOwnerGroupId(), common.getOwnerUserId()));
 //		}
 		double discount = 0.0d;
-		User_jur user = null;
-		if (!getInitiator().isAnonimous())
-			user = User_jur.get(new ItemQuery(ItemNames.USER_JUR).setUser(getInitiator()).loadFirstItem());
+		User_jur user = User_jur.get(new ItemQuery(ItemNames.USER_JUR).setUser(getInitiator()).loadFirstItem());
 		if (user != null) {
 			discount += user.getDefault_discount(0.0d);
 		}
@@ -123,30 +116,30 @@ public class CartManageCommand extends BasicCartManageCommand {
 			if (uf != null) {
 				String shipType = uf.getStringValue("ship_type");
 				if (StringUtils.containsIgnoreCase(shipType, "самовывоз")) {
-//					discount += discounts.get_self_delivery();
+					//discount += discounts.get_self_delivery();
 				} else if (StringUtils.containsIgnoreCase(shipType, "автолайт")) {
-//					discount -= discounts.get_autolight();
+					//discount -= discounts.get_autolight();
 				} else if (StringUtils.containsIgnoreCase(shipType, "доставка")) {
-//					discount -= discounts.get_delivery();
+					//discount -= discounts.get_delivery();
 				}
 				if (StringUtils.containsIgnoreCase(uf.getStringValue("pay_type"), "предоплата")) {
-//					discount += discounts.get_pay_first();
+					//discount += discounts.get_pay_first();
 				}
 			}
 		}
 		BigDecimal discountedSum = originalSum.multiply(new BigDecimal((100 - discount) / 100));
 		discountedSum = discountedSum.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-		cart.setValue(ItemNames.cart_.SUM_DISCOUNT, discountedSum);
+		//cart.setValue(ItemNames.cart_.SUM_DISCOUNT, discountedSum);
 		// Сумма прописью
 		BigDecimal rub = discountedSum.setScale(0, BigDecimal.ROUND_FLOOR);
 		BigDecimal kop = discountedSum.subtract(rub).multiply(new BigDecimal(100)).setScale(0, BigDecimal.ROUND_CEILING);
-		String sumText = Strings.numberToRusWords(rub.doubleValue()) + " "
-				+ Strings.numberEnding(rub.doubleValue(), "белорусский рубль", "белорусских рубля", "белорусских рублей")
-				+ " " + kop + " "
-				+ Strings.numberEnding(kop.doubleValue(), "копейка", "копейки", "копеек");
-		String dateStr = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(new Locale("ru")).format(LocalDate.now());
-		cart.setValueUI(ItemNames.cart_.EXTRA_SUM_STR, sumText);
-		cart.setValueUI(ItemNames.cart_.EXTRA_DATE_STR, dateStr);
+		//String sumText = Strings.numberToRusWords(rub.doubleValue()) + " "
+		//		+ Strings.numberEnding(rub.doubleValue(), "белорусский рубль", "белорусских рубля", "белорусских рублей")
+		//		+ " " + kop + " "
+		//		+ Strings.numberEnding(kop.doubleValue(), "копейка", "копейки", "копеек");
+		//String dateStr = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(new Locale("ru")).format(LocalDate.now());
+		//cart.setValueUI(ItemNames.cart_.EXTRA_SUM_STR, sumText);
+		//cart.setValueUI(ItemNames.cart_.EXTRA_DATE_STR, dateStr);
 		getSessionMapper().saveTemporaryItem(cart);
 		return success;
 	}
@@ -200,6 +193,4 @@ public class CartManageCommand extends BasicCartManageCommand {
 			return false;
 		}
 	}
-	*/
-
 }
