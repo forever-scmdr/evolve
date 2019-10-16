4ab1cd9b7d8379015005d9dba83e3b54270c1f7a
anton <bio@forever-ds.com>
2019-10-16 16:29:03 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

---
+++ src/extra/FixKeyUnique.java
@@ -0,0 +1,50 @@
+package extra;
+
+import ecommander.fwk.Strings;
+import ecommander.fwk.integration.CatalogConst;
+import ecommander.model.Item;
+import ecommander.pages.Command;
+import ecommander.pages.ResultPE;
+import ecommander.persistence.commandunits.SaveItemDBUnit;
+import ecommander.persistence.mappers.ItemMapper;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class FixKeyUnique extends Command implements CatalogConst {
+
+	@Override
+	public ResultPE execute() throws Exception {
+		processItems(PRODUCT_ITEM);
+		processItems(SECTION_ITEM);
+		return getResult("success");
+	}
+
+	private void processItems(String itemName) throws Exception {
+		LinkedList<Item> itemsToFix = new LinkedList<Item>();
+		long id = -1;
+		itemsToFix.addAll(ItemMapper.loadByName(itemName, 1000, id));
+		HashSet<String> existingKeys = new HashSet();
+		while (itemsToFix.size() > 0) {
+			Item product;
+			while ((product = itemsToFix.poll()) != null) {
+				id = product.getId();
+				String keyUnique = product.getKeyUnique();
+				String expectedKeyUnique = StringUtils.substring(Strings.translit(product.getKey()), 0, 98);
+				if ((!keyUnique.equals(expectedKeyUnique) && !keyUnique.equals(expectedKeyUnique + id)) || existingKeys.contains(keyUnique)) {
+					if (existingKeys.contains(expectedKeyUnique)) {
+						product.setKeyUnique(expectedKeyUnique + id);
+					}else {
+						product.setKeyUnique(expectedKeyUnique);
+						existingKeys.add(expectedKeyUnique);
+					}
+					executeAndCommitCommandUnits(SaveItemDBUnit.get(product).noTriggerExtra().noFulltextIndex().ignoreFileErrors().ignoreUser());
+				}else{
+					existingKeys.add(keyUnique);
+				}
+			}
+			itemsToFix.addAll(ItemMapper.loadByName(itemName, 1000, id));
+		}
+	}
+}
