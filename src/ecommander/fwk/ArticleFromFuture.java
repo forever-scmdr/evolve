2b6d0a88ede3c8fbe03840c92e436aaf4842249a
anton <bio@forever-ds.com>
2020-02-03 15:30:50 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/ecommander/fwk/ArticleFromFuture.java
+++ src/ecommander/fwk/ArticleFromFuture.java
@@ -3,27 +3,48 @@ package ecommander.fwk;
 import ecommander.model.Item;
 import ecommander.pages.Command;
 import ecommander.pages.ResultPE;
-import ecommander.persistence.commandunits.SaveItemDBUnit;
+import ecommander.persistence.commandunits.DBPersistenceCommandUnit;
+import ecommander.persistence.commandunits.ItemStatusDBUnit;
 import ecommander.persistence.common.PersistenceCommandUnit;
+import ecommander.persistence.itemquery.ItemQuery;
+import ecommander.persistence.mappers.LuceneIndexMapper;
 import extra._generated.ItemNames;
 
 import java.util.Date;
+import java.util.LinkedList;
 
 public class ArticleFromFuture extends Command implements ItemEventCommandFactory {
     @Override
     public PersistenceCommandUnit createCommand(Item item) throws Exception {
         long date = item.getLongValue(ItemNames.news_item_.DATE,0);
         long now = new Date().getTime();
+        DBPersistenceCommandUnit commandUnit = null;
         if(date > now){
-            item.setValueUI("is_future", "1");
+           commandUnit = ItemStatusDBUnit.hide(item);
         }else{
-            item.setValueUI("is_future", "0");
+           commandUnit = ItemStatusDBUnit.restore(item);
         }
-        return SaveItemDBUnit.get(item).noTriggerExtra().noFulltextIndex();
+        return commandUnit;
     }
 
-    public ResultPE checkFuture(){
+    public ResultPE checkFuture() throws Exception {
+        LinkedList<Item> articles = new LinkedList<Item>();
+//        long before = new Date().getTime() - (86400*1000);
+        long now = new Date().getTime();
+        ItemQuery q = new ItemQuery("small_news_item", Item.STATUS_HIDDEN);
+        articles.addAll(q.loadItems());
+        q = new ItemQuery("news_item", Item.STATUS_HIDDEN);
+        articles.addAll(q.loadItems());
 
+        boolean needReindex = false;
+        for(Item a : articles){
+            long v = a.getLongValue(ItemNames.news_item_.DATE, 0);
+            if(v < now){
+                executeAndCommitCommandUnits(ItemStatusDBUnit.restore(a).ignoreUser(true));
+                needReindex = true;
+            }
+        }
+        if(needReindex) LuceneIndexMapper.getSingleton().reindexAll();
         return null;
     }
 
