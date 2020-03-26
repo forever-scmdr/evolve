75ecd9eec61ba49a3f2ca0df3a91139b2c5b6a01
anton <bio@forever-ds.com>
2020-03-26 14:14:23 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

---
+++ src/extra/AddRandomAlso.java
@@ -0,0 +1,73 @@
+package extra;
+
+import ecommander.fwk.IntegrateBase;
+import ecommander.fwk.integration.CatalogConst;
+import ecommander.model.Item;
+import ecommander.persistence.commandunits.SaveItemDBUnit;
+import ecommander.persistence.itemquery.ItemQuery;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddRandomAlso extends IntegrateBase implements CatalogConst {
+	@Override
+	protected boolean makePreparations() throws Exception {
+		return true;
+	}
+
+	@Override
+	protected void integrate() throws Exception {
+		setOperation("Загрузка разделов");
+		LinkedList<Item> allSections = new LinkedList<>();
+		allSections.addAll(new ItemQuery(SECTION_ITEM).loadItems());
+		Item currentSection;
+		setOperation("Добавление кодов похожих товаров");
+		while((currentSection = allSections.poll()) != null){
+			info.setCurrentJob(currentSection.getStringValue(NAME));
+			processSection(currentSection);
+		}
+	}
+
+	private void processSection(Item currentSection) throws Exception {
+		List<Item> products = new ItemQuery(PRODUCT_ITEM).setParentId(currentSection.getId(), false).loadItems();
+		if(products.size() == 0) return;
+		else if(products.size() < 7){
+			for(Item p : products){
+				p.clearValue(SIMILAR_CODE_PARAM);
+				for(Item ass : products){
+					if(p == ass) continue;
+					p.setValue(SIMILAR_CODE_PARAM, ass.getValue(CODE_PARAM));
+				}
+				executeAndCommitCommandUnits(SaveItemDBUnit.get(p).noFulltextIndex().ignoreUser().noTriggerExtra().ignoreFileErrors());
+				info.increaseProcessed();
+			}
+		}else{
+			ArrayList<Integer> idx = new ArrayList<>();
+			for(int i= 0; i < products.size(); i++){
+				idx.add(i);
+			}
+			for(Item p : products){
+				p.clearValue(SIMILAR_CODE_PARAM);
+				Collections.shuffle(idx);
+				int s = 0;
+				for(int j = 0; j < 7 && s < 6; j++){
+					Item ass = products.get(idx.get(j));
+					if(p == ass) continue;
+					p.setValue(SIMILAR_CODE_PARAM, ass.getValue(CODE_PARAM));
+					s++;
+				}
+				executeAndCommitCommandUnits(SaveItemDBUnit.get(p).noFulltextIndex().ignoreUser().noTriggerExtra().ignoreFileErrors());
+				info.increaseProcessed();
+			}
+		}
+		info.pushLog("Готово ("+products.size()+"): " +currentSection.getStringValue(NAME));
+	}
+
+
+	@Override
+	protected void terminate() throws Exception {
+
+	}
+}
