5987f7d5c1ab862f1489a2d4394d99ffeae2ddda
anton <bio@forever-ds.com>
2020-12-07 14:35:00 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/ecommander/fwk/integration/YMarketCreateXMLFile.java
+++ src/ecommander/fwk/integration/YMarketCreateXMLFile.java
@@ -128,6 +128,8 @@ public class YMarketCreateXMLFile extends Command implements CatalogConst {
 				String url = base + "/" + baseProduct.getKeyUnique() + "/";
 				xml.startElement(URL_ELEMENT).addText(url).endElement();
 				xml.startElement(PRICE_ELEMENT).addText(product.getDecimalValue(PRICE_PARAM)).endElement();
+				//patch 07.12.2020 <option cost="10" days="2 or 40"/>
+				xml.startElement("option", "days", product.getByteValue(AVAILABLE_PARAM, (byte)0) > 0? 2 : 40, "cost", 10).endElement();
 				xml.startElement(CURRENCY_ID_ELEMENT).addText("BYN").endElement();
 				xml.startElement(CATEGORY_ID_ELEMENT).addText(category.getId()).endElement();
 				String name = baseProduct.getStringValue(NAME_PARAM);
