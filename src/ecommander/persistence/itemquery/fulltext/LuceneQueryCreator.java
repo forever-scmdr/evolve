329a6bf6751a9cb807b076f30a542423aefde9b5
Egor <georgy.karlov@gmail.com>
2021-01-05 17:12:21 +0300

    Фиксы полнотекстового поиска (KeywordAnalyzer, заглавные буквы)

--- src/ecommander/persistence/itemquery/fulltext/LuceneQueryCreator.java
+++ src/ecommander/persistence/itemquery/fulltext/LuceneQueryCreator.java
@@ -1,11 +1,10 @@
 package ecommander.persistence.itemquery.fulltext;
 
+import ecommander.persistence.mappers.LuceneIndexMapper;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.DisjunctionMaxQuery;
 import org.apache.lucene.search.Query;
-
-import ecommander.persistence.mappers.LuceneIndexMapper;
 
 import java.util.ArrayList;
 
@@ -17,17 +16,23 @@ import java.util.ArrayList;
 public abstract class LuceneQueryCreator {
 
 	public Query createLuceneQuery(String queryStr, String[] paramNames, Occur occur) {
-		QueryParser parser = LuceneIndexMapper.createQueryParser(paramNames[0]);
-		parser.setAllowLeadingWildcard(true);
 		if (paramNames.length == 1) {
+			QueryParser parser = createQueryParser(paramNames[0]);
 			return createQuery(parser, paramNames[0], queryStr, occur);
 		}
 		ArrayList<Query> queries = new ArrayList<>();
 		for (String paramName : paramNames) {
+			QueryParser parser = createQueryParser(paramName);
 			queries.add(createQuery(parser, paramName, queryStr, occur));
 		}
 		return new DisjunctionMaxQuery(queries, 0.1f);
 	}
-	
+
+	protected QueryParser createQueryParser(String paramName) {
+		QueryParser parser = LuceneIndexMapper.createQueryParser(paramName);
+		parser.setAllowLeadingWildcard(true);
+		return parser;
+	}
+
 	protected abstract Query createQuery(QueryParser parser, String param, String value, Occur occur);
 }
