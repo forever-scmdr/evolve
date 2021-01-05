f3c6cd52e34027955fe3c50bdc0685c58cf27154
anton <bio@forever-ds.com>
2021-01-05 14:30:45 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/extra/FarnellSearchCommand.java
+++ src/extra/FarnellSearchCommand.java
@@ -21,6 +21,9 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,6 +35,7 @@ public class FarnellSearchCommand extends Command implements CatalogConst {
 		put("callInfo.apiKey", "nqwuz6kvr25knx8v7q3eyjm7");
 		put("resultsSettings.responseGroup", "large");
 		put("versionNumber", "1.1");
+		put("userInfo.customerId", "epasschipru");
 	}};
 
 	private static final String IN_STOCK_AND_ROHS = "resultsSettings.refinements.filters";
@@ -40,6 +44,10 @@ public class FarnellSearchCommand extends Command implements CatalogConst {
 	private static final String NUMBER_OF_RESULTS = "resultsSettings.numberOfResults";
 	private static final String TERM = "term";
 	private static final String ENTITY_DECLARATION = "\n<!DOCTYPE farnell_products [<!ENTITY nbsp \"&#160;\">]>\n";
+	private static final String SIGNATURE_VAR = "userInfo.signature";
+	private static final String TIMESTAMP_VAR = "userInfo.timestamp";
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm:ss.SSS");
 
 	private Document doc;
 	private Element variables;
@@ -73,6 +81,14 @@ public class FarnellSearchCommand extends Command implements CatalogConst {
 		String query = URLEncoder.encode(':' + getVarSingleValue("q"), StandardCharsets.UTF_8.toString());
 
 		urlBuilder.append('&').append(TERM).append('=').append("any").append(query);
+
+		LocalDateTime nowUTC = LocalDateTime.now(ZoneId.of("UTC"));
+		String formattedNow = DATE_FORMATTER.format(nowUTC)+'T'+TIME_FORMATTER.format(nowUTC);
+		urlBuilder.append('&').append(TIMESTAMP_VAR).append('=').append(formattedNow);
+
+		String signature = generateSignature(formattedNow);
+		urlBuilder.append('&').append(SIGNATURE_VAR).append('=').append(signature);
+
 		System.out.println(urlBuilder);
 
 		doc = Jsoup.parse(new URL(urlBuilder.toString()), 5000);
@@ -100,6 +116,15 @@ public class FarnellSearchCommand extends Command implements CatalogConst {
 		output = output.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + ENTITY_DECLARATION);
 		result.setValue(output);
 		return result;
+	}
+
+	private String generateSignature(String formattedNow) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException{
+		String toEncode = "searchByKeyword" + formattedNow;
+		SecretKeySpec keySpec = new SecretKeySpec("8iRUet0c7csU".getBytes("UTF-8"), "HmacSHA1");
+		Mac mac = Mac.getInstance("HmacSHA1");
+		mac.init(keySpec);
+		String hash = Base64.encodeBase64String(mac.doFinal(toEncode.getBytes()));
+		return URLEncoder.encode(hash, "UTF-8");
 	}
 
 	/**
