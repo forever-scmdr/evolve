990ef011c9e3da6c3e32e8576f7a40dec217b0f2
Egor <georgy.karlov@gmail.com>
2021-01-10 20:03:52 +0300

    Фиксы полнотекстового поиска (KeywordAnalyzer, заглавные буквы), страница поиска

--- src/ecommander/persistence/itemquery/fulltext/LuceneQueryCreator.java
+++ src/ecommander/persistence/itemquery/fulltext/LuceneQueryCreator.java
@@ -17,17 +17,23 @@ import java.util.ArrayList;
 public abstract class LuceneQueryCreator {
 
 	public Query createLuceneQuery(String queryStr, String[] paramNames, Occur occur) {
-		QueryParser parser = LuceneIndexMapper.createQueryParser(paramNames[0]);
-		parser.setAllowLeadingWildcard(true);
 		if (paramNames.length == 1) {
+			QueryParser parser = createQueryParser(paramNames[0]);
 			return createQuery(parser, paramNames[0], queryStr, occur);
 		}
 		ArrayList<Query> queries = new ArrayList<>();
 		for (String paramName : paramNames) {
+			QueryParser parser = createQueryParser(paramName);
 			queries.add(createQuery(parser, paramName, queryStr, occur));
 		}
 		return new DisjunctionMaxQuery(queries, 0.1f);
 	}
-	
+
+	protected QueryParser createQueryParser(String paramName) {
+		QueryParser parser = LuceneIndexMapper.createQueryParser(paramName);
+		parser.setAllowLeadingWildcard(true);
+		return parser;
+	}
+
 	protected abstract Query createQuery(QueryParser parser, String param, String value, Occur occur);
 }
