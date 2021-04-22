6170b60132580e54a9ecf64eeddfa73fb8a05360
anton <bio@forever-ds.com>
2021-04-22 16:14:02 +0300

    Merge remote-tracking branch 'origin/tme' into svetilniki
    # Conflicts:
    #	src/ecommander/fwk/integration/CreateExcelPriceList.java
    #	src/ecommander/fwk/integration/ImportProductsFromExcel.java
    #	web/META-INF/context.xml
    #	web/WEB-INF/ec_xml/model.xml
    #	web/WEB-INF/ec_xml/model_custom.xml
    #	web/WEB-INF/ec_xml/pages.xml
    #	web/WEB-INF/ec_xml/~model.xml
    #	web/WEB-INF/ec_xml/~model_custom.xml

--- src/ecommander/controllers/ScheduledJob.java
+++ src/ecommander/controllers/ScheduledJob.java
@@ -1,16 +1,5 @@
 package ecommander.controllers;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-
-import org.apache.commons.lang3.StringUtils;
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
 import ecommander.fwk.ServerLogger;
 import ecommander.fwk.XmlDocumentBuilder;
 import ecommander.model.datatypes.DateDataType;
@@ -18,6 +7,19 @@ import ecommander.pages.ExecutablePagePE;
 import ecommander.pages.PageModelRegistry;
 import ecommander.pages.ResultPE;
 import ecommander.pages.ResultPE.ResultType;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Date;
+
 /**
  * Класс, который выполняет определенную страницу, имеющую команды, согласно расписанию
  * Quartz
@@ -30,7 +32,8 @@ public class ScheduledJob implements Job {
 	public static final String PAGE_NAME = "page_name";
 	public static final String JOB_NAME_PREFIX = "exec_page_";
 	public static final String TRIGGER_PREFIX = "trigger_";
-	
+	public static final long LIFESPAN = 24 * 10 * 3600 * 1000;
+
 	public void execute(JobExecutionContext ctx) throws JobExecutionException {
 		String pageName = ctx.getJobDetail().getJobDataMap().getString(PAGE_NAME);
 		ServerLogger.debug("Start scheduled job page '" + pageName + "'");
@@ -53,6 +56,16 @@ public class ScheduledJob implements Job {
 						+ DateDataType.outputDate(System.currentTimeMillis(), DateDataType.REPORT_FORMATTER) + ".html";
 				File reportFile = new File(reportFileName);
 				reportFile.getParentFile().mkdirs();
+
+				File[] oldFiles = reportFile.getParentFile().listFiles();
+				long now = new Date().getTime();
+
+				for(File old : oldFiles){
+					if(now - old.lastModified() > LIFESPAN){
+						FileUtils.deleteQuietly(old);
+					}
+				}
+
 				Files.write(reportFile.toPath(), out.toByteArray(), StandardOpenOption.WRITE, StandardOpenOption.CREATE,
 						StandardOpenOption.TRUNCATE_EXISTING);
 			}
