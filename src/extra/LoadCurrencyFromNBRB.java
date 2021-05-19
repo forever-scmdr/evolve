d613ab6401f615e64c5ba22d8e554c841b03945b
anton <bio@forever-ds.com>
2021-05-19 12:48:36 +0300

    mobile menu fix

--- src/extra/LoadCurrencyFromNBRB.java
+++ src/extra/LoadCurrencyFromNBRB.java
@@ -8,11 +8,13 @@ import ecommander.persistence.commandunits.SaveItemDBUnit;
 import ecommander.persistence.itemquery.ItemQuery;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class LoadCurrencyFromNBRB extends Command implements CatalogConst {
 	private static final String NBNB_URL = "http://www.nbrb.by/Services/XmlExRates.aspx?ondate=";
@@ -20,21 +22,22 @@ public class LoadCurrencyFromNBRB extends Command implements CatalogConst {
 
 	@Override
 	public ResultPE execute() throws Exception {
+		List<Item> currencies = new ItemQuery("currency").loadItems();
+		if(currencies.size() == 0){
+			return getResult("success");
+		}
 		Document doc = Jsoup.parse(new URL(NBNB_URL + FORMAT.format(new Date())), 5000);
-		Elements els = doc.getElementsByAttributeValue("Id", "298");
-		String ratio = els.first().select("Rate").html();
-
-		Elements usd = doc.getElementsByAttributeValue("Id", "145");
-		String usdRatio = usd.first().select("Rate").html();
-
-		Elements eur = doc.getElementsByAttributeValue("Id", "292");
-		String eurRatio = eur.first().select("Rate").html();
-
-		Item catalog = ItemQuery.loadSingleItemByName(CATALOG_ITEM);
-		catalog.setValueUI("currency_ratio", ratio);
-		catalog.setValueUI("currency_ratio_usd", usdRatio);
-		catalog.setValueUI("currency_ratio_eur", eurRatio);
-		executeAndCommitCommandUnits(SaveItemDBUnit.get(catalog).ignoreUser().ignoreFileErrors().noFulltextIndex());
+		for (Item currency : currencies){
+			Elements infoFromBank = doc.getElementsContainingOwnText(currency.getStringValue(NAME));
+			Element curs = infoFromBank.first().parent();
+			String scale = curs.select("Scale").text();
+			String rate = curs.select("Rate").text();
+			String title = curs.select("Name").text();
+			currency.setValueUI("scale", scale);
+			currency.setValueUI("ratio", rate);
+			currency.setValueUI("title", title);
+			executeAndCommitCommandUnits(SaveItemDBUnit.get(currency).ignoreUser(true));
+		}
 		return getResult("success");
 	}
 }
