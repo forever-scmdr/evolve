52af7836bb562b906a3a880ad5e91a6bfea2d6aa
anton <bio@forever-ds.com>
2021-06-11 14:57:13 +0300

    mobile menu fix

--- src/ecommander/fwk/WebClient.java
+++ src/ecommander/fwk/WebClient.java
@@ -288,6 +288,24 @@ public class WebClient implements Closeable {
 	}
 
 	/**
+	 * Сохранить файл без сохранения сеанса с сервером источником файла
+	 * @param url
+	 * @param dirName
+	 * @param saveAs
+	 * @param proxy
+	 * @throws IOException
+	 * @throws KeyStoreException
+	 * @throws NoSuchAlgorithmException
+	 * @throws KeyManagementException
+	 */
+	public static void saveFileQuick(String url, String dirName, String saveAs, String...proxy)
+			throws IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+		try (WebClient client = WebClient.newClient()) {
+			client.saveFileWithClient(url, dirName, saveAs, null, proxy);
+		}
+	}
+
+	/**
 	 * Сохранить файл с сохранением сеанса с сервером источником файла
 	 * @param url
 	 * @param dirName
