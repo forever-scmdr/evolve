1d5873771d2dd70b87440611b6e617fe4145f045
anton <bio@forever-ds.com>
2021-10-19 16:27:31 +0300

    belchip visual fixes

--- src/extra/CreateOzonFeed.java
+++ src/extra/CreateOzonFeed.java
@@ -15,14 +15,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class CreateOzonFeed extends IntegrateBase implements CatalogConst {
 
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-YYYY_HH-mm-ss");
     private static final String OUTPUT_FOLDER = AppContext.getFilesDirPath(false);
-    private static final String REGEX = "ozon_feed_\\d{2}-\\d{2}-\\d{4}_\\d{2}-\\d{2}-\\d{2}\\.xml";
+    private static final String REGEX = "ozon_feed_.xml";
     private XmlDocumentBuilder doc = XmlDocumentBuilder.newDoc();
 
     @Override
@@ -58,7 +57,7 @@ public class CreateOzonFeed extends IntegrateBase implements CatalogConst {
         }
         doc.endElement().endElement().endElement();
 
-        String fileName = String.format("ozon_feed_%s.xml", DATE_FORMAT.format(new Date()));
+        String fileName = "ozon_feed_.xml";
         Path outputFile = Paths.get(OUTPUT_FOLDER, fileName);
         Files.write(outputFile, doc.toString().getBytes(StandardCharsets.UTF_8));
         pushLog(getUrlBase()+"/files/"+fileName);
