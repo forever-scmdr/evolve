4c9b33bad8ad1f41697727d4196df571015c284d
anton <bio@forever-ds.com>
2022-09-28 19:31:26 +0300

    - ec_xsl updated to production state

--- src/ecommander/fwk/external_shops/AbstractLoadAdditionalContentCommand.java
+++ src/ecommander/fwk/external_shops/AbstractLoadAdditionalContentCommand.java
@@ -8,7 +8,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 public abstract class AbstractLoadAdditionalContentCommand extends Command {
 
@@ -21,11 +25,30 @@ public abstract class AbstractLoadAdditionalContentCommand extends Command {
 		if(urls != null){
 			Item externalInfo = getSessionMapper().getSingleItemByParamValue("external_url", "code", code);
 			if(externalInfo != null){
-				content = externalInfo.getStringValue("content", content);
+				//content = externalInfo.getStringValue("content", content);
 				if(StringUtils.isBlank(content)){
 					String url = externalInfo.getStringValue("url");
-					Document html = Jsoup.parse(new URL(url), 25000);
-					content = extractContent(html);
+
+					StringBuilder sb = new StringBuilder();
+					String line;
+
+					URLConnection urlConnection = new URL(url).openConnection();
+					urlConnection.addRequestProperty("User-Agent", "Mozilla");
+					urlConnection.setReadTimeout(5000);
+					urlConnection.setConnectTimeout(5000);
+
+					try (InputStream is = urlConnection.getInputStream();
+						 BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+
+						while ((line = br.readLine()) != null) {
+							sb.append(line);
+						}
+
+					}
+
+					//String html = FluentWebClient.getString(url);
+
+					content = extractContent(Jsoup.parse(sb.toString()));
 					externalInfo.setValueUI("content", content);
 					getSessionMapper().saveTemporaryItem(externalInfo);
 				}
@@ -33,6 +56,7 @@ public abstract class AbstractLoadAdditionalContentCommand extends Command {
 		}
 		return defineResult(content);
 	}
+
 
 	protected abstract String extractContent(Document html);
 
