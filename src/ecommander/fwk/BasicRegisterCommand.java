970f566796c82f0e8c8546c5a6c5abfb86466bb2
eeee <egor@forever-ds.com>
2024-09-12 19:09:33 +0300

    Новая команда регистрации (отправка писем)

--- src/ecommander/fwk/BasicRegisterCommand.java
+++ src/ecommander/fwk/BasicRegisterCommand.java
@@ -8,6 +8,8 @@ import ecommander.persistence.commandunits.SaveItemDBUnit;
 import ecommander.persistence.commandunits.SaveNewUserDBUnit;
 import ecommander.persistence.commandunits.UpdateUserDBUnit;
 import ecommander.persistence.itemquery.ItemQuery;
+import extra.CartManageCommand;
+import extra._generated.ItemNames;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.mail.Multipart;
@@ -15,6 +17,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMultipart;
 import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
+import java.util.HashSet;
 
 /**
  * Команда для регистрации пользователя
@@ -183,5 +186,59 @@ public abstract class BasicRegisterCommand extends Command {
 		}
 	}
 
-	protected abstract boolean validate() throws Exception;
+	/**
+	 * Валидация - простая проверка полей на заполненность
+	 * @return
+	 * @throws Exception
+	 */
+	protected boolean validate() throws Exception {
+		Item form = getItemForm().getItemSingleTransient();
+		boolean isPhys = form.getTypeId() == ItemTypeRegistry.getItemType(ItemNames.USER_PHYS).getTypeId();
+		boolean hasError = false;
+		if (isPhys) {
+			String mandatoryPhys = getVarSingleValue("mandatory_phys");
+			for (String mandatory : getMandatory(mandatoryPhys, ItemNames.USER_PHYS, CartManageCommand.MANDATORY_PHYS)) {
+				if (form.isValueEmpty(mandatory)) {
+					getItemForm().setValidationError(form.getId(), mandatory, "Не заполнен параметр");
+					hasError = true;
+				}
+			}
+			removeSessionForm("register");
+			saveSessionForm("register");
+		} else {
+			String mandatoryJur = getVarSingleValue("mandatory_jur");
+			for (String mandatory : getMandatory(mandatoryJur, ItemNames.USER_JUR,CartManageCommand.MANDATORY_JUR)) {
+				if (form.isValueEmpty(mandatory)) {
+					getItemForm().setValidationError(form.getId(), mandatory, "Не заполнен параметр");
+					hasError = true;
+				}
+			}
+			removeSessionForm("register");
+			saveSessionForm("register");
+		}
+		return !hasError;
+	}
+
+	/**
+	 * Обязательные поля для заданного айтема, передается название айтема, переменная с полями и поля по умолчанию
+	 * @param mandatoryVarValue
+	 * @param itemName
+	 * @param defaultMandatory
+	 * @return
+	 */
+	public static HashSet<String> getMandatory(String mandatoryVarValue, String itemName, HashSet<String> defaultMandatory) {
+		HashSet<String> mandatorySet = new HashSet<>();
+		if (StringUtils.isNotBlank(mandatoryVarValue)) {
+			String[] params = StringUtils.split(mandatoryVarValue, ",; ");
+			ItemType type = ItemTypeRegistry.getItemType(itemName);
+			for (String param : params) {
+				if (type.hasParameter(param)) {
+					mandatorySet.add(param);
+				}
+			}
+		} else {
+			mandatorySet.addAll(defaultMandatory);
+		}
+		return  mandatorySet;
+	}
 }
